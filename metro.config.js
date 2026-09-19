fbcca6bc3670e7a9dcf0ba3263d4c9a8fd64895e
Andreas Tolnes <andreas.tolnes@soprasteria.com>
2026-09-19 12:15:10 +0200

    Exclude staging credentials from Metro environment loading

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,9 @@
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 const config = getSentryExpoConfig(__dirname);
-config.resolver.blockList = [/[/\\]sveltemo[/\\].*/];
+config.resolver.blockList = [
+  ...config.resolver.blockList,
+  /[/\\]sveltemo[/\\].*/,
+  // Deployment credentials are CLI inputs, not Expo environment modules.
+  /[/\\]\.env\.staging\.local$/,
+];
 module.exports = config;
