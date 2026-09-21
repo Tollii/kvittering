54cf5e25a5008becb06a6ecc13b88b5916e5c564
Andreas Tolnes <andreas.tolnes@soprasteria.com>
2026-09-21 10:43:08 +0200

    chore: remove the retired prototype lint exclusion

--- eslint.config.js
+++ eslint.config.js
@@ -13,8 +13,6 @@ module.exports = defineConfig([
   {
     ignores: [
       "sveltemo/**",
-      // Standalone design studies are archived references, not application code.
-      "plans/cobalt-design-prototype/**",
       "convex/_generated/**",
       "convex/kassalapp/generated/**",
       "tools/oxlint/anti-slop/**",
